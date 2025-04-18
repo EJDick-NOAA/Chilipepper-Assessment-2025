81ee542e84fa34e324206d0a49e5725dc128f6dc
EJDick-NOAA <64495990+EJDick-NOAA@users.noreply.github.com>
2025-04-18 12:48:39 -0700

    models 15-16
    fix error message; model doesn't like it when all forecast catches are set to zero

---
+++ models/Model 016, make forecast greater than 0 to debug IT WORKED/r4ss_script.R
@@ -0,0 +1,23 @@
+# test r4ss installation
+
+#install.packages("remotes")
+#library(remotes)
+#remotes::install_github("r4ss/r4ss")
+
+library(r4ss)
+setwd('E:/ejdick/projects/~Chilipepper 2025/model/Model 014, add comm trawl logbook index (lambda=0); sdrep errors')
+
+# NO HESSIAN
+myreplist <- SS_output(dir=getwd(), covar=FALSE)
+tune_comps(myreplist, option="Francis")
+SS_plots(replist = myreplist, datplot=TRUE, ptsize=8)
+
+# REQUIRES HESSIAN
+myreplist <- SS_output(dir=getwd(), covar=TRUE) # need covar to estimate bias ramp
+#SS_fitbiasramp(myreplist, print=TRUE)
+tune_comps(myreplist, option="Francis")
+SS_plots(replist = myreplist, datplot=TRUE, ptsize=8)
+
+
+# Exec Summ tables
+SSexecutivesummary(myreplist) # creates a 'tables' directory
